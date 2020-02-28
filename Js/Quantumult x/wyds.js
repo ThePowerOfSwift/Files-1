404f24560bbd3d127d6893af94cc901d2fef2fd2
0 or 1 <mmick2@student.ccc.edu>
2020-02-28 16:37:44 +0800

    .

---
+++ Js/Quantumult x/wyds.js
@@ -0,0 +1,33 @@
+/*
+    
+surge:
+
+http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js,script-update-interval=0
+
+[URL Rewrite]
+
+^https?://p\.du\.163\.com/readtime/info.json - reject
+
+QX:
+
+^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body wnyd.js
+
+[mimt]
+
+hostname=p.du.163.com
+
+*/
+
+
+
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.tradeEndTime = 1679685290000;
+body = JSON.stringify(obj);
+$done({body});
+
+
+
+
---
+++ Js/Quantumult x/youtube.surge.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+var data = {
+  body: "{}",
+  headers: {
+    "Content-Type": "multipart/byteranges"
+  }
+};
+$done({response: data});
