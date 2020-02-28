06ce184428f57bda4fb5ad1bc6684d2a707c4986
0 or 1 <mmick2@student.ccc.edu>
2020-02-29 00:08:23 +0800

    .

---
+++ Js/Surge/Bilibili.js
@@ -0,0 +1,16 @@
+
+
+Surge
+
+[URL Rewrite]
+
+
+https:\/\/api\.bilibili\.com\/pgc\/player\/api\/playurl http://api.bili.best:22333/geturl/ 302
+
+
+
+https:\/\/api.bilibili.com\/pgc\/view\/app\/season http://api.bili.best:22333/season/ 302
+
+
+mitm = api.bilibili.com
+
